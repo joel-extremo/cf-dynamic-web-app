ac0e1b4f3820a684e677fe1843c0f142739502fd
Jose Manuel Rodriguez <jmrv002@gmail.com>
2018-10-11 04:36:00 -0400

    change color to colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_105500) do
+ActiveRecord::Schema.define(version: 2018_10_11_083104) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_105500) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
     t.float "price"
   end
 
